64ac21fefcabcacabd3482f1644d481392ef11ac
nonameable <ddelgado2013@gmail.com>
2017-02-23 09:02:41 -0500

    added database connection

--- server.js
+++ server.js
@@ -16,8 +16,16 @@ app.get('/', function(req, res) {
 res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
 });
 
-app.listen(config.port, () => {
-  console.log('listening on 1337')
-})
-
 // Database connection
+
+MongoClient.connect(config.database, (err, database) => {
+
+  if (err) return console.log(err)
+  db = database;
+	console.log('me conecte bien a la base de datos de no crime');
+  module.exports.database = db;
+
+  app.listen(config.port, () => {
+    console.log('listening on 1337')
+  })
+});
