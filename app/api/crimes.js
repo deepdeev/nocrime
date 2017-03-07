b31a16f5210c7f1d4af05d701a08ba4deb6c1ca7
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-03-06 21:12:58 -0500

    Code review by Nicolás Gómez to David Delgado

--- app/api/crimes.js
+++ app/api/crimes.js
@@ -4,6 +4,12 @@ var path = require('path');
 
 
 // I need an instance of db in this file, so I can call it when I need to add something
+// ngomez22
+// No me parece correcto importar todo el servidor aquí solo para obtener
+// la base de datos.
+// Tal vez podrían crear un módulo aparte para funciones de la base de datos,
+// como retornar la conexión.
+
 var server = require('../../server');
 var db;
 
@@ -35,6 +41,8 @@ router.post('/',  function(req, res){
 
 });
 
+// ngomez22
+// Tal vez este método podría ser un GET ?
 router.post('/search', function (req, res){
   var search = req.body;
   if(Object.getOwnPropertyNames(search).length < 3){
